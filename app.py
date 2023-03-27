821e6625cf472ccbdb3e9f01f5498873240acb2e
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-03-27 20:29:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from google.oauth2 import service_account
 from gsheetsdb import connect
 import pandas as pd
 
+st.set_page_config(page_title = 'Facebook Ad Campaign Dashboard',
+                    layout='wide',
+                    initial_sidebar_state='collapsed')
+
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
