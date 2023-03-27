8569ebaf3bbca7fa8ca363fe9038b6f1de07ce27
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-03-27 20:32:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def run_query(query):
 sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-st.title("Streamlit Demo Dashboard)
+st.title("Streamlit Demo Dashboard")
 
 # # Print results.
 # for row in rows:
