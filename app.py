9ea2f279105c4d65c5fd4b604891189a49bce8e2
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-03-27 13:30:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,6 @@ def run_query(query):
 sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-# Print results.
-for row in rows:
-    st.write(rows)
+# # Print results.
+# for row in rows:
+st.write(rows)
