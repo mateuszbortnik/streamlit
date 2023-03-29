aa9d7649b9d8b1f55e727a78585761d8eb550f2f
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-03-29 17:50:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -65,10 +65,11 @@ with col1:
   # Filter the dataset based on the date slider values
   filtered_data = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
 
-  st.metric(label="Revenue", value=filtered_data['Google_Ads_Revenue_Total'].sum().round(decimals=2))
+  st.metric(label="Revenue (USD)", value=filtered_data['Google_Ads_Revenue_Total'].sum().round(decimals=2))
   
   # Plot the line chart
   st.line_chart(filtered_data, x='Date', y=options)
 
 with col2:
-  st.text("col2")
+
+  st.metric(label="Spend (USD)", value=filtered_data['Google_Ads_Spend_Total'].sum().round(decimals=2))
