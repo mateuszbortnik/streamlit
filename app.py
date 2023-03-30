54771c63f5bfca5138f3977f6193d147317570cd
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-03-30 15:44:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -87,3 +87,10 @@ import plotly.express as px
 fig = px.bar(df, x="Date", y=["Google_Ads_Revenue_Total", "Facebook_Ads_Revenue_Total"], title="Wide-Form Input")
 mygrid[4][0].plotly_chart(fig, theme="streamlit", use_container_width=True)
 mygrid[4][1].plotly_chart(fig, theme="streamlit")
+
+fig.update_layout(legend=dict(
+    yanchor="top",
+    y=0.99,
+    xanchor="left",
+    x=0.01
+))
