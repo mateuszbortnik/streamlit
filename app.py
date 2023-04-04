ac48538662691fc465cce2be498c7509eee6d3b0
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2023-04-04 16:00:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ mygrid[4][2].bar_chart(data=filtered_data, x="Year_and_month", y=["Google_Ads_Co
 # sns.heatmap(filtered_data.corr(), ax=ax)
 # st.write(fig4)
 
-xaxis = st.selectbox(
+xaxis = mygrid[5][0].selectbox(
     'Select spend channel',
     ('Facebook_Ads_Spend_Total', 'Facebook_Ads_Spend_Prospecting', 'Facebook_Ads_Spend_Remarketing_Brand_Nurturing', 'Facebook_Ads_Spend_not_set', 'Google_Ads_Spend_Total', 'Google_Ads_Spend_Prospecting', 'Google_Ads_Spend_Remarketing_Brand_Nurturing', 'Google_Ads_Spend_not_set'))
 
